cb064e104e46ededd2f95790822f7aee787a3091
Wilmer Cuevas <wilmerjesus442002@gmail.com>
2026-08-16 21:05:50 -0300

    Added formats-data file

---
+++ data/mods/venova/formats-data.ts
@@ -0,0 +1,527 @@
+export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = {
+  "bullchub": {
+    "tier": "OU"
+  },
+  "bullboss": {
+    "tier": "OU"
+  },
+  "bulltauron": {
+    "tier": "OU"
+  },
+  "flamare": {
+    "tier": "OU"
+  },
+  "flamanda": {
+    "tier": "OU"
+  },
+  "flamarza": {
+    "tier": "OU"
+  },
+  "nagasy": {
+    "tier": "OU"
+  },
+  "trisailor": {
+    "tier": "OU"
+  },
+  "triseidon": {
+    "tier": "OU"
+  },
+  "moruga": {
+    "tier": "OU"
+  },
+  "morphel": {
+    "tier": "OU"
+  },
+  "morphelia": {
+    "tier": "OU"
+  },
+  "guaby": {
+    "tier": "OU"
+  },
+  "guabird": {
+    "tier": "OU"
+  },
+  "gualady": {
+    "tier": "OU"
+  },
+  "gualord": {
+    "tier": "OU"
+  },
+  "pialtric": {
+    "tier": "OU"
+  },
+  "gigatric": {
+    "tier": "OU"
+  },
+  "rabibald": {
+    "tier": "OU"
+  },
+  "rabibare": {
+    "tier": "OU"
+  },
+  "chygir": {
+    "tier": "OU"
+  },
+  "chygarp": {
+    "tier": "OU"
+  },
+  "colochama": {
+    "tier": "OU"
+  },
+  "grandchama": {
+    "tier": "OU"
+  },
+  "lapitta": {
+    "tier": "OU"
+  },
+  "pedialora": {
+    "tier": "OU"
+  },
+  "cachilatt": {
+    "tier": "OU"
+  },
+  "cachitaw": {
+    "tier": "OU"
+  },
+  "igualeaf": {
+    "tier": "OU"
+  },
+  "iguatree": {
+    "tier": "OU"
+  },
+  "iguasauro": {
+    "tier": "OU"
+  },
+  "cunagy": {
+    "tier": "OU"
+  },
+  "cuniwer": {
+    "tier": "OU"
+  },
+  "cunapoar": {
+    "tier": "OU"
+  },
+  "mangry": {
+    "tier": "OU"
+  },
+  "manggave": {
+    "tier": "OU"
+  },
+  "polana": {
+    "tier": "OU"
+  },
+  "poliood": {
+    "tier": "OU"
+  },
+  "lycotar": {
+    "tier": "OU"
+  },
+  "tarantor": {
+    "tier": "OU"
+  },
+  "bantza": {
+    "tier": "OU"
+  },
+  "bantdor": {
+    "tier": "OU"
+  },
+  "venamillar": {
+    "tier": "OU"
+  },
+  "venapivar": {
+    "tier": "OU"
+  },
+  "zagami": {
+    "tier": "OU"
+  },
+  "motorrist": {
+    "tier": "OU"
+  },
+  "morroan": {
+    "tier": "OU"
+  },
+  "cardilack": {
+    "tier": "OU"
+  },
+  "cardelan": {
+    "tier": "OU"
+  },
+  "rockaima": {
+    "tier": "OU"
+  },
+  "rockicer": {
+    "tier": "OU"
+  },
+  "rockiman": {
+    "tier": "OU"
+  },
+  "tonifin": {
+    "tier": "OU"
+  },
+  "tonitrich": {
+    "tier": "OU"
+  },
+  "toniptera": {
+    "tier": "OU"
+  },
+  "danpir": {
+    "tier": "OU"
+  },
+  "daneast": {
+    "tier": "OU"
+  },
+  "boaconda": {
+    "tier": "OU"
+  },
+  "hydraconda": {
+    "tier": "OU"
+  },
+  "mareepv": {
+    "tier": "OU"
+  },
+  "flaaffyv": {
+    "tier": "OU"
+  },
+  "ampharosv": {
+    "tier": "OU"
+  },
+  "raltsv": {
+    "tier": "OU"
+  },
+  "kirliav": {
+    "tier": "OU"
+  },
+  "gardevoirv": {
+    "tier": "OU"
+  },
+  "galladev": {
+    "tier": "OU"
+  },
+  "ezely": {
+    "tier": "OU"
+  },
+  "ezengyl": {
+    "tier": "OU"
+  },
+  "monalatt": {
+    "tier": "OU"
+  },
+  "monalou": {
+    "tier": "OU"
+  },
+  "monguato": {
+    "tier": "OU"
+  },
+  "wailmerv": {
+    "tier": "OU"
+  },
+  "wailordv": {
+    "tier": "OU"
+  },
+  "chigoat": {
+    "tier": "OU"
+  },
+  "chemtorn": {
+    "tier": "OU"
+  },
+  "fautorn": {
+    "tier": "OU"
+  },
+  "ponytav": {
+    "tier": "OU"
+  },
+  "rapidashv": {
+    "tier": "OU"
+  },
+  "motiti": {
+    "tier": "OU"
+  },
+  "motilos": {
+    "tier": "OU"
+  },
+  "aloenca": {
+    "tier": "OU"
+  },
+  "saloe": {
+    "tier": "OU"
+  },
+  "heralea": {
+    "tier": "OU"
+  },
+  "ocypy": {
+    "tier": "OU"
+  },
+  "ocycry": {
+    "tier": "OU"
+  },
+  "hormoso": {
+    "tier": "OU"
+  },
+  "hormiwar": {
+    "tier": "OU"
+  },
+  "knock": {
+    "tier": "OU"
+  },
+  "petroger": {
+    "tier": "OU"
+  },
+  "jumpter": {
+    "tier": "OU"
+  },
+  "jumrock": {
+    "tier": "OU"
+  },
+  "lucarb": {
+    "tier": "OU"
+  },
+  "sayolda": {
+    "tier": "OU"
+  },
+  "genteat": {
+    "tier": "OU"
+  },
+  "siljarion": {
+    "tier": "OU"
+  },
+  "pavidig": {
+    "tier": "OU"
+  },
+  "pavicuc": {
+    "tier": "OU"
+  },
+  "apimar": {
+    "tier": "OU"
+  },
+  "arpaqueen": {
+    "tier": "OU"
+  },
+  "cacryc": {
+    "tier": "OU"
+  },
+  "cacryso": {
+    "tier": "OU"
+  },
+  "formiga": {
+    "tier": "OU"
+  },
+  "formdier": {
+    "tier": "OU"
+  },
+  "attadier": {
+    "tier": "OU"
+  },
+  "penma": {
+    "tier": "OU"
+  },
+  "pendrina": {
+    "tier": "OU"
+  },
+  "pelrad": {
+    "tier": "OU"
+  },
+  "tacarita": {
+    "tier": "OU"
+  },
+  "tacaribe": {
+    "tier": "OU"
+  },
+  "pumevalt": {
+    "tier": "OU"
+  },
+  "frontie": {
+    "tier": "OU"
+  },
+  "frontmer": {
+    "tier": "OU"
+  },
+  "trushia": {
+    "tier": "OU"
+  },
+  "truzuhe": {
+    "tier": "OU"
+  },
+  "coldor": {
+    "tier": "OU"
+  },
+  "comboldor": {
+    "tier": "OU"
+  },
+  "sardfist": {
+    "tier": "OU"
+  },
+  "sardboned": {
+    "tier": "OU"
+  },
+  "mutin": {
+    "tier": "OU"
+  },
+  "munevar": {
+    "tier": "OU"
+  },
+  "sunkernv": {
+    "tier": "OU"
+  },
+  "sunflorav": {
+    "tier": "OU"
+  },
+  "anthispa": {
+    "tier": "OU"
+  },
+  "vespir": {
+    "tier": "OU"
+  },
+  "vespidon": {
+    "tier": "OU"
+  },
+  "piravitar": {
+    "tier": "OU"
+  },
+  "torchela": {
+    "tier": "OU"
+  },
+  "zanance": {
+    "tier": "OU"
+  },
+  "cairoco": {
+    "tier": "OU"
+  },
+  "cairaba": {
+    "tier": "OU"
+  },
+  "maskevy": {
+    "tier": "OU"
+  },
+  "genico": {
+    "tier": "OU"
+  },
+  "ranty": {
+    "tier": "OU"
+  },
+  "rantal": {
+    "tier": "OU"
+  },
+  "ransap": {
+    "tier": "OU"
+  },
+  "raned": {
+    "tier": "OU"
+  },
+  "ranpsi": {
+    "tier": "OU"
+  },
+  "ranark": {
+    "tier": "OU"
+  },
+  "ranelva": {
+    "tier": "OU"
+  },
+  "smoochoomv": {
+    "tier": "OU"
+  },
+  "jynxv": {
+    "tier": "OU"
+  },
+  "quintoil": {
+    "tier": "OU"
+  },
+  "tachitoil": {
+    "tier": "OU"
+  },
+  "snoruntv": {
+    "tier": "OU"
+  },
+  "glaliev": {
+    "tier": "OU"
+  },
+  "froslassv": {
+    "tier": "OU"
+  },
+  "igglybuffv": {
+    "tier": "OU"
+  },
+  "jigglypuffv": {
+    "tier": "OU"
+  },
+  "wigglytuffv": {
+    "tier": "OU"
+  },
+  "zacarth": {
+    "tier": "OU"
+  },
+  "faidy": {
+    "tier": "OU"
+  },
+  "beldumv": {
+    "tier": "OU"
+  },
+  "metangv": {
+    "tier": "OU"
+  },
+  "metagrossv": {
+    "tier": "OU"
+  },
+  "diyare": {
+    "tier": "OU"
+  },
+  "yaredra": {
+    "tier": "OU"
+  },
+  "yaregon": {
+    "tier": "OU"
+  },
+  "reymoth": {
+    "tier": "OU"
+  },
+  "aguikari": {
+    "tier": "OU"
+  },
+  "aguipit": {
+    "tier": "OU"
+  },
+  "aguixut": {
+    "tier": "OU"
+  },
+  "aguixem": {
+    "tier": "OU"
+  },
+  "elebeon": {
+    "tier": "OU"
+  },
+  "cainebre": {
+    "tier": "OU"
+  },
+  "arpidor": {
+    "tier": "OU"
+  },
+  "orquicess": {
+    "tier": "OU"
+  },
+  "vairam": {
+    "tier": "OU"
+  },
+  "canamate": {
+    "tier": "OU"
+  },
+  "tugekko": {
+    "tier": "OU"
+  },
+  "tugekang": {
+    "tier": "OU"
+  },
+  "onzanterian": {
+    "tier": "OU"
+  },
+  "simobolite": {
+    "tier": "OU"
+  },
+  "sazonte": {
+    "tier": "OU"
+  },
+  "answy": {
+    "tier": "OU"
+  },
+  "antelum": {
+    "tier": "OU"
+  }
+};
